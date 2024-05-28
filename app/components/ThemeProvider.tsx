8587c5085d55c355fbbd57f1f71533d5ff902184
nzkks <kkshanthosh@gmail.com>
2024-05-29 11:40:49 +1200

    Refactor theme switching to be compatible with Shadcn-UI

--- app/components/ThemeProvider.tsx
+++ app/components/ThemeProvider.tsx
@@ -1,31 +1,18 @@
 'use client';
 
-import { ReactNode, useEffect, useState } from 'react';
-import ThemeContext from '../context/themeContext';
+import * as React from 'react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { type ThemeProviderProps } from 'next-themes/dist/types';
 
-const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const themeFromLocalStorage: boolean =
-    typeof localStorage !== 'undefined' && localStorage.getItem('site-theme')
-      ? JSON.parse(localStorage.getItem('site-theme')!)
-      : false;
-
-  const [darkTheme, setDarkTheme] = useState<boolean>(themeFromLocalStorage);
-  const [renderComponent, setRenderComponent] = useState<boolean>(false);
-
-  useEffect(() => {
-    setRenderComponent(true);
-  }, []);
-
-  if (!renderComponent) return <></>;
-
+const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
   return (
-    <ThemeContext.Provider value={{ darkTheme, setDarkTheme }}>
-      <div className={`${darkTheme ? 'dark' : ''} min-h-screen`}>
+    <NextThemesProvider {...props}>
+      <div className="min-h-screen">
         <div className="text-[#1e1e1e] dark:bg-black dark:text-white">
           {children}
         </div>
       </div>
-    </ThemeContext.Provider>
+    </NextThemesProvider>
   );
 };
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           <Navbar />
           {children}
         </ThemeProvider>
