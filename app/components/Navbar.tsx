9f9aa413d093b7547d6246b28902e464bd741641
nzkks <kkshanthosh@gmail.com>
2024-05-28 16:11:32 +1200

    Navbar: Once logged in, get the user info from the Kinde server session

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,12 +4,16 @@ import {
   LoginLink,
   RegisterLink,
 } from '@kinde-oss/kinde-auth-nextjs/components';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
 import { Button } from '@/components/ui/button';
 import NavbarLinks from './NavbarLinks';
 import MobileMenu from './MobileMenu';
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
   return (
     <nav className="relative mx-auto flex w-full max-w-7xl items-center px-4 py-7 md:grid md:grid-cols-12 md:px-8">
       <div className="md:col-span-3">
@@ -23,18 +27,24 @@ const Navbar = () => {
       <NavbarLinks />
 
       <div className="ms-auto flex items-center gap-x-2 md:col-span-3">
-        <Button size="icon" asChild>
-          <LoginLink>
-            <LogIn className="h-4 w-4" />
-          </LoginLink>
-        </Button>
-        <Button variant="secondary">
-          <RegisterLink>Register</RegisterLink>
-        </Button>
+        {user ? (
+          <h1>Hi {user.given_name}</h1>
+        ) : (
+          <div className="flex items-center gap-x-2">
+            <Button size="icon" asChild>
+              <LoginLink>
+                <LogIn className="h-4 w-4" />
+              </LoginLink>
+            </Button>
+            <Button variant="secondary">
+              <RegisterLink>Register</RegisterLink>
+            </Button>
 
-        <div className="md:hidden">
-          <MobileMenu />
-        </div>
+            <div className="md:hidden">
+              <MobileMenu />
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
