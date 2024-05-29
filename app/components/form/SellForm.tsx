eb5ce4dac82b7b9652412c5bd41f91202e8df126
nzkks <kkshanthosh@gmail.com>
2024-05-29 14:47:38 +1200

    Sellform: minor

--- app/components/form/SellForm.tsx
+++ app/components/form/SellForm.tsx
@@ -7,9 +7,9 @@ import {
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
+import { UploadDropzone } from '@/app/lib/uploadthing';
 import SelectCategory from '../SelectCategory';
 import WYSIWYGEditor from '../WYSIWYGEditor';
-import { UploadDropzone } from '@/app/lib/uploadthing';
 
 const SellForm = () => {
   return (
@@ -19,60 +19,60 @@ const SellForm = () => {
         <CardDescription>
           Please describe your product here in detail so that it can be sold
         </CardDescription>
-        <CardContent className="flex flex-col gap-y-10">
-          <div className="flex flex-col gap-y-2">
-            <Label>Name</Label>
-            <Input
-              name="name"
-              type="text"
-              placeholder="Name of your Product"
-              required
-              minLength={3}
-            />
-          </div>
-
-          <div className="flex flex-col gap-y-2">
-            <Label>Category</Label>
-            <SelectCategory />
-          </div>
-
-          <div className="flex flex-col gap-y-2">
-            <Label>Price</Label>
-            <Input
-              placeholder="29$"
-              type="number"
-              name="price"
-              required
-              min={1}
-            />
-          </div>
-
-          <div className="flex flex-col gap-y-2">
-            <Label>Small Summary</Label>
-            <Textarea
-              name="smallDescription"
-              placeholder="Pleae describe your product shortly right here..."
-              required
-              minLength={10}
-            />
-          </div>
-
-          <div className="flex flex-col gap-y-2">
-            <Label>Description</Label>
-            <WYSIWYGEditor />
-          </div>
-
-          <div className="flex flex-col gap-y-2">
-            <Label>Product Images</Label>
-            <UploadDropzone endpoint="imageUploader" />
-          </div>
-
-          <div className="flex flex-col gap-y-2">
-            <Label>Product File</Label>
-            <UploadDropzone endpoint="productFileUploader" />
-          </div>
-        </CardContent>
       </CardHeader>
+      <CardContent className="flex flex-col gap-y-10">
+        <div className="flex flex-col gap-y-2">
+          <Label>Name</Label>
+          <Input
+            name="name"
+            type="text"
+            placeholder="Name of your Product"
+            required
+            minLength={3}
+          />
+        </div>
+
+        <div className="flex flex-col gap-y-2">
+          <Label>Category</Label>
+          <SelectCategory />
+        </div>
+
+        <div className="flex flex-col gap-y-2">
+          <Label>Price</Label>
+          <Input
+            placeholder="29$"
+            type="number"
+            name="price"
+            required
+            min={1}
+          />
+        </div>
+
+        <div className="flex flex-col gap-y-2">
+          <Label>Small Summary</Label>
+          <Textarea
+            name="smallDescription"
+            placeholder="Pleae describe your product shortly right here..."
+            required
+            minLength={10}
+          />
+        </div>
+
+        <div className="flex flex-col gap-y-2">
+          <Label>Description</Label>
+          <WYSIWYGEditor />
+        </div>
+
+        <div className="flex flex-col gap-y-2">
+          <Label>Product Images</Label>
+          <UploadDropzone endpoint="imageUploader" />
+        </div>
+
+        <div className="flex flex-col gap-y-2">
+          <Label>Product File</Label>
+          <UploadDropzone endpoint="productFileUploader" />
+        </div>
+      </CardContent>
     </form>
   );
 };
