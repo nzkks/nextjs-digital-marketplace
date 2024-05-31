cf172661a5c4f6384a845225065c013fa6acb4a2
nzkks <kkshanthosh@gmail.com>
2024-05-31 21:33:10 +1200

    minor

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,5 +1,6 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-import { createUploadthing, type FileRouter } from 'uploadthing/next';
+import { createUploadthing } from 'uploadthing/next';
+import type { FileRouter } from 'uploadthing/next';
 import { UploadThingError } from 'uploadthing/server';
 
 const f = createUploadthing();
