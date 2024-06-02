02dab9bc1c10542cec0d0ee7ea80e7cec9d23257
nzkks <kkshanthosh@gmail.com>
2024-06-02 22:11:55 +1200

    Add a Loading skeleton for Sell route page

---
+++ app/sell/loading.tsx
@@ -0,0 +1,16 @@
+import { Card, CardHeader } from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
+
+const loading = () => {
+  return (
+    <div className="mx-auto max-w-7xl px-4 md:px-8">
+      <Card>
+        <CardHeader className="h-[1000px]">
+          <Skeleton className="h-full w-full" />
+        </CardHeader>
+      </Card>
+    </div>
+  );
+};
+
+export default loading;
