38e5f6a286d632841a00c32bdaf3131ca6a3d772
nzkks <kkshanthosh@gmail.com>
2024-06-03 10:12:00 +1200

    Sell route page: make it a protected route

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,7 +1,13 @@
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { Card } from '@/components/ui/card';
 import SellForm from '../components/form/SellForm';
 
-const page = () => {
+const page = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) throw new Error('Not authorized...');
+
   return (
     <section className="mx-auto mb-14 max-w-7xl px-4 md:px-8">
       <Card>
