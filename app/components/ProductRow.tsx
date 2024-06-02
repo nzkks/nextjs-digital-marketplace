c0249149a3d034307f4f02e78ce753f56ca225f2
nzkks <kkshanthosh@gmail.com>
2024-06-02 15:32:38 +1200

    ProductRow: Fix the title, row and link text

--- app/components/ProductRow.tsx
+++ app/components/ProductRow.tsx
@@ -100,24 +100,24 @@ const getData = async ({ category }: Props) => {
 };
 
 const ProductRow = async ({ category }: Props) => {
-  const { data: products } = await getData({ category });
+  const data = await getData({ category });
 
   return (
     <section className="mt-12">
       <div className="md:flex md:items-center md:justify-between">
         <h2 className="text-2xl font-extrabold tracking-tighter ">
-          Newest Products
+          {data.title}
         </h2>
         <Link
-          href="#"
+          href={data.link}
           className="hidden text-sm font-medium text-primary hover:text-primary/90 md:block"
         >
-          All Products <span>&rarr;</span>
+          All {data.title} <span>&rarr;</span>
         </Link>
       </div>
 
       <div className="gird-cols-1 mt-4 grid gap-10 sm:grid-cols-2 lg:grid-cols-3">
-        {products.map((product) => (
+        {data.data.map((product) => (
           <ProductCard key={product.id} {...product} />
         ))}
       </div>
